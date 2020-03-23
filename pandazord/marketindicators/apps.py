a00afb5355b6d04bf9fe41b89b8372b0b8d19988
marcusmello <mandealista@gmail.com>
2020-03-23 17:00:14 -0300

    Market indicators app, django automatic created files

---
+++ pandazord/marketindicators/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MarketindicatorsConfig(AppConfig):
+    name = 'marketindicators'
---
+++ pandazord/marketindicators/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+#TODO: Docstrings and Type annotations
+
+from .common_libs import PriceSeriesFrom
+
+class Trend:
+
+    def __init__(self, klines):
+
+        self.klines = klines
+
+
+    def simple_moving_average(self, price_source, number_of_samples):
+
+        price = getattr(PriceSeriesFrom(self.klines), price_source + '_')()
+
+        return price.rolling(number_of_samples).mean()
+
+
+class Momentum:
+    pass
+
+
+class Volatility:
+    pass
+
+
+class Volume:
+    pass
