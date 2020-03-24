77e0f80c8dfe2513c0a0b20c8fedee7a72e95e81
marcusmello <mandealista@gmail.com>
2020-03-24 17:59:11 -0300

    Added apps (signalgenerator and controlpanel) and templates

--- pandazord/core/settings.py
+++ pandazord/core/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sidedefiner',
+    'controlpanel',
+    'signalgenerators',
     'django_extensions',
 ]
 
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
