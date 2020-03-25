a7af93a26554ec10a60b78f4e77272e9377b9657
marcusmello <mandealista@gmail.com>
2020-03-25 20:47:47 -0300

    Added 'Exchange' entry

--- pandazord/controlpanel/admin.py
+++ pandazord/controlpanel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BinanceAssetsControl
+from .models import Exchange, BinanceAssetsControl
 
 # Register your models here.
 admin.site.register(BinanceAssetsControl)
+admin.site.register(Exchange)
